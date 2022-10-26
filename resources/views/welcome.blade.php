9f994a58ae8d58ee6118b4352f3d16a4259fc271
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-10-26 09:25:00 -0400

    added react frontend, have not got it to work yet;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,14 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
+    <div id="root"></div>
 @endsection
 
 @section('scripts')
     @parent
-
+    <!--below is what routes react into this blade file-->
+    <script src="{{asset('js/app.js')}}"></script>
     <script>
-        actions.TitleBar.create(app, { title: 'Welcome' });
+        actions.TitleBar.create(app, { title: 'Howdy' });
     </script>
 @endsection
